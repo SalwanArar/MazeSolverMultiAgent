e6aad66b225d8b37bcd23c5fff8a9a17c3f9461d
Stefan Stoleriu <s.stoleriu99@gmail.com>
2025-01-02 15:46:01 +0200

    added try to move command

--- PlanetAgent.cs
+++ PlanetAgent.cs
@@ -5,6 +5,11 @@ namespace MazeGeneration
     public class PlanetAgent : Agent
     {
         public static int[,] MazeGrid { get; private set; }
+        public Dictionary<string, string> AgentPosition { get; set; }
+        public enum AgentState { NotSpawned, Spawned, Dead };
+        public Dictionary<string, AgentState> AgentStates { get; set; }
+
+        private bool isExitFound = false;
         public event Action MazeGenerated; // Event to signal maze completion
 
         public override void Setup()
@@ -17,7 +22,7 @@ namespace MazeGeneration
                 for (int j = 0; j < Utils.Size; j++)
                     MazeGrid[i, j] = 1; // All walls initially
 
-            GenerateMazeWithPrim();
+            Utils.GenerateMazeWithPrim();
 
             Console.WriteLine("Maze generation complete.");
             for (int i = 0; i < MazeGrid.GetLength(0); i++)
@@ -30,6 +35,83 @@ namespace MazeGeneration
             }
 
             MazeGenerated?.Invoke();
+        }
+
+        public override void Act(Message message) 
+        {
+            Console.WriteLine("\t[{1} -> {0}]: {2}", this.Name, message.Sender, message.Content);
+            
+            string action; 
+            string parameters;
+            Utils.ParseMessage(message.Content, out action, out parameters);
+        
+            switch (action)
+            {
+                case "try_move":
+                    HandleTryMove(message.Sender, parameters);
+                    break;
+                default:
+                    break;
+            }
+
+            //need to update GUI
+        }
+
+        private void HandleTryMove(string sender, string position)
+        {
+            //position = "0 1"
+            Point point = Utils.parsePosition(position);
+
+            if (MazeGrid[point.x, point.y] == 1) //it's wall
+            {
+                Send(sender, "block");
+                return;
+            }
+
+            // if it's another agent
+            foreach (string agent in AgentPosition.Keys)
+            {
+                if (agent == sender)
+                    continue;
+                if (AgentPosition[agent] == position)
+                {
+                    // Besides just block, send also the explorer's name to comunicate the state.
+                    Send(sender, Utils.Str("block", agent));
+                    return;
+                }
+            }
+
+            AgentPosition[sender] = position;
+
+            if (position == exitPosition) // Cwhat is the exitPosition?
+            {
+                string messageType = isExitFound ? "exit" : "found";
+                Send(sender, Utils.Str(messageType, position));
+
+                if (!isExitFound)
+                {
+                    isExitFound = true;
+                }
+
+                // Mark the explorer as dead and remove it from the active list
+                AgentStates[sender] = AgentState.Dead;
+                AgentPosition.Remove(sender);
+
+                Console.WriteLine("Remaining Explorers: {0}", AgentPosition.Count);
+
+                // If no explorers remain, handle stopping the process
+                if (ExplorerPositions.Count == 0)
+                {
+                    Console.WriteLine("{0}: Stopped", Name);
+                    foreach (string agent in Environment.AllAgents())
+                    {
+                        Console.WriteLine("Remaining agent: {0}", agent);
+                    }
+                    this.Stop();
+                }
+                return;
+            }            
+            Send(sender, Utils.Str("move", position));
         }
     }
 }
--- Utils.cs
+++ Utils.cs
@@ -1,5 +1,11 @@
 ﻿namespace MazeGeneration
 {
+    struct Point 
+    {
+        public int x;
+        public int y;
+    }
+
     public static class Utils
     {
         public static int Size = 24;
@@ -23,7 +29,7 @@
 
             int startX = random.Next(n);
             int startY = random.Next(n);
-            MazeGrid[startX, startY] = 0;
+            PlanetAgent.MazeGrid[startX, startY] = 0;
             visited[startX, startY] = true;
 
             AddWallsToQueue(startX, startY, edges, visited);
@@ -40,8 +46,8 @@
                 AddWallsToQueue(x, y, edges, visited);
             }
 
-            MazeGrid[0, 1] = 0;
-            MazeGrid[n - 1, n - 2] = 0;
+            PlanetAgent.MazeGrid[0, 1] = 0;
+            PlanetAgent.MazeGrid[n - 1, n - 2] = 0;
         }
 
         private void AddWallsToQueue(int x, int y, SortedSet<(int weight, int x, int y, int fromX, int fromY)> edges, bool[,] visited)
@@ -66,6 +72,35 @@
                     edges.Add((weight, newX, newY, x, y));
                 }
             }
+        }
+
+        public static void ParseMessage(string content, out string action, out string parameters)
+        {
+            string[] t = content.Split();
+
+            action = t[0];
+
+            parameters = "";
+
+            if (t.Length > 1)
+            {
+                for (int i = 1; i < t.Length - 1; i++)
+                    parameters += t[i] + " ";
+                parameters += t[t.Length - 1];
+            }
+        }
+
+        public Point parsePosition(string position) {
+            string[] t = position.Split();
+            Point p;
+            p.x = int.Parse(t[0]);
+            p.y = int.Parse(t[1]);
+            return p;
+        }
+
+        public static string Str(object p1, object p2)
+        {
+            return string.Format("{0} {1}", p1, p2);
         }
     }
 }
